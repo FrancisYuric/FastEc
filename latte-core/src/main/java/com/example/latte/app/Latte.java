09d65fdf3c65b4cfdf45ccbc3071a251891d2964
Gogs <gogs@fake.local>
2018-01-07 22:33:21 +0800

    create Latte.class

---
+++ latte-core/src/main/java/com/example/latte/app/Latte.java
@@ -0,0 +1,12 @@
+package com.example.latte.app;
+
+/**
+ * Created by xushiyun on 2018/1/7.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.app
+ * File Name:    Latte
+ * Descripetion: Todo
+ */
+
+public class Latte {
+}
