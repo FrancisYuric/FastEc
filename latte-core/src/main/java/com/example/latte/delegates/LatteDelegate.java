52496661153e84259507392e889956e3ab494ee3
Gogs <gogs@fake.local>
2018-01-08 16:22:56 +0800

    new LatteDelegate extends PermissionCheckerDelegate

---
+++ latte-core/src/main/java/com/example/latte/delegates/LatteDelegate.java
@@ -0,0 +1,12 @@
+package com.example.latte.delegates;
+
+/**
+ * Created by xushiyun on 2018/1/8.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.delegates
+ * File Name:    LatteDelegate
+ * Descripetion: Todo
+ */
+
+public abstract class LatteDelegate extends PermissionCheckerDelegate {
+}
---
+++ latte-core/src/main/java/com/example/latte/delegates/PermissionCheckerDelegate.java
@@ -0,0 +1,13 @@
+package com.example.latte.delegates;
+
+/**
+ * Created by xushiyun on 2018/1/8.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.delegates
+ * File Name:    PermissionCheckerDelegate
+ * Descripetion: Todo
+ */
+
+//解决android6.0前后的动态权限的问题
+public abstract class PermissionCheckerDelegate extends BaseDelegate {
+}
