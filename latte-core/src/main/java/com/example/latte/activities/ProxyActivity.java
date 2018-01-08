5732d4ef63c7bcfc6ed1f80ab48daeaae930caf9
Gogs <gogs@fake.local>
2018-01-08 16:06:30 +0800

    butterknife集成前

---
+++ latte-core/src/main/java/com/example/latte/activities/ProxyActivity.java
@@ -0,0 +1,14 @@
+package com.example.latte.activities;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by xushiyun on 2018/1/8.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.activities
+ * File Name:    ProxyActivity
+ * Descripetion: Todo
+ */
+
+public class ProxyActivity extends SupportActivity {
+}
