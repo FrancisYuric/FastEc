c9be715919e6f51b8c3bd8ee1c0b3dc9b8c318df
Gogs <gogs@fake.local>
2018-01-09 13:29:14 +0800

    test RestClient

--- example/src/main/java/com/example/xushiyun/fastec/ExampleDelegate.java
+++ example/src/main/java/com/example/xushiyun/fastec/ExampleDelegate.java
@@ -5,6 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.example.latte.delegates.LatteDelegate;
+import com.example.latte.net.RestClient;
+import com.example.latte.net.callback.IError;
+import com.example.latte.net.callback.IFailure;
+import com.example.latte.net.callback.ISuccess;
 
 /**
  * Created by xushiyun on 2018/1/8.
@@ -23,5 +27,30 @@ public class ExampleDelegate extends LatteDelegate {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
         //对每一个控件进行操作
+    }
+
+    private void testRestClient() {
+        RestClient.builder()
+                .url("")
+                .params("", "")
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+
+                    }
+                })
+                .faliure(new IFailure() {
+                    @Override
+                    public void onFailure() {
+
+                    }
+                })
+                .error(new IError() {
+                    @Override
+                    public void onError(int code, String msg) {
+
+                    }
+                })
+                .build();
     }
 }
