1f0df02c54feef6780aa8a303bfb2e3800079839
Gogs <gogs@fake.local>
2018-01-09 15:00:02 +0800

    设置用于显示加载图像的弹出框,并设置自定义样式

---
+++ latte-core/src/main/java/com/example/latte/ui/LatteLoader.java
@@ -0,0 +1,18 @@
+package com.example.latte.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+/**
+ * Created by xushiyun on 2018/1/9.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.ui
+ * File Name:    LatteLoader
+ * Descripetion: Todo
+ */
+
+public class LatteLoader {
+    public static void showLoading(Context context, String type) {
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+    }
+}
