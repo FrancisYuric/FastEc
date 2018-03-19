41f8e1891aa704d700fb93f54b453710163e7f85
ciruy <398712739@qq.com>
2018-03-20 00:25:07 +0800

    创建LatteWeChat类

---
+++ latte-core/src/main/java/com/example/latte/wechat/BaseWXActivity.java
@@ -0,0 +1,24 @@
+package com.example.latte.wechat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+
+/**
+ * Created by xushiyun on 2018/3/20.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.wechat
+ * File Name:    BaseWXActivity
+ * Description: Todo
+ */
+
+public abstract class BaseWXActivity extends AppCompatActivity implements IWXAPIEventHandler {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //这个必须写在onCreate中
+
+    }
+}
--- latte-core/src/main/java/com/example/latte/wechat/LatteWeChat.java
+++ latte-core/src/main/java/com/example/latte/wechat/LatteWeChat.java
@@ -1,5 +1,13 @@
 package com.example.latte.wechat;
 
+import android.app.Activity;
+
+import com.example.latte.app.ConfigKeys;
+import com.example.latte.app.Latte;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
 /**
  * Created by xushiyun on 2018/3/20.
  * Project Name: FastEc
@@ -9,5 +17,28 @@ package com.example.latte.wechat;
  */
 
 public class LatteWeChat {
+    static final String APP_ID = Latte.getConfiguration(ConfigKeys.WE_CHAT_APP_ID);
+    static final String APP_SECRET = Latte.getConfiguration(ConfigKeys.WE_CHAT_APP_SECRET);
+    private final IWXAPI WXAPI;
 
+    private static final class Holder {
+        private static final LatteWeChat INSTANCE = new LatteWeChat();
+    }
+
+    private LatteWeChat() {
+        final Activity activity = Latte.getConfiguration(ConfigKeys.ACTIVITY);
+        WXAPI = WXAPIFactory.createWXAPI(activity, APP_ID, true);
+        WXAPI.registerApp(APP_ID);
+    }
+
+    public final IWXAPI getWXAPI() {
+        return WXAPI;
+    }
+
+    public final void signIn() {
+        final SendAuth.Req req = new SendAuth.Req();
+        req.scope = "snsapi_userinfo";
+        req.state = "random_state";
+        WXAPI.sendReq(req);
+    }
 }
