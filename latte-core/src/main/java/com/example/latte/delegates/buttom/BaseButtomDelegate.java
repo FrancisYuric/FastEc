d41eee948bd4640f0428b2e5a016bca7bf1a5c00
ciruy <398712739@qq.com>
2018-03-20 11:35:06 +0800

    创建BottomItemDelegate

---
+++ latte-core/src/main/java/com/example/latte/delegates/buttom/BaseButtomDelegate.java
@@ -0,0 +1,14 @@
+package com.example.latte.delegates.buttom;
+
+import com.example.latte.delegates.LatteDelegate;
+
+/**
+ * Created by xushiyun on 2018/3/20.
+ * Project Name: FastEc
+ * Package Name: com.example.latte.delegates.buttom
+ * File Name:    BaseButtomDelegate
+ * Description: Todo
+ */
+
+public abstract class BaseButtomDelegate extends LatteDelegate {
+}
