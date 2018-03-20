5eaf6322f37337d694f68a79a065ef9bb228d0f5
ciruy <398712739@qq.com>
2018-03-20 14:26:28 +0800

    IndexDelegate初步设计

--- latte-ec/src/main/java/com/example/latte/ec/main/index/IndexDelegate.java
+++ latte-ec/src/main/java/com/example/latte/ec/main/index/IndexDelegate.java
@@ -2,10 +2,22 @@ package com.example.latte.ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.latte.delegates.buttom.BottomItemDelegate;
 import com.example.latte.ec.R;
+import com.example.latte.ec.R2;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by xushiyun on 2018/3/20.
@@ -16,6 +28,19 @@ import com.example.latte.ec.R;
  */
 
 public class IndexDelegate extends BottomItemDelegate {
+
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
@@ -24,5 +49,19 @@ public class IndexDelegate extends BottomItemDelegate {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
